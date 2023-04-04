2014e8c9e604f967023ef1b709fd08e2c824b658
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-04-04 10:52:57 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 
+let name:string;
+name = "seyedssss";
+
+let age:number;
+age = 24;
+
+let student:boolean;
+student = true;
+
+let hobbies:string[]; // array of string
+let hobbie:number[]; // array of number
+
+
 function App() {
   return (
     <div className="App">
-      {"seyed"}
+      {name}
+      <br/>
+      {age}
+      <br/>
+      {student}
+
       
     </div>
   );
